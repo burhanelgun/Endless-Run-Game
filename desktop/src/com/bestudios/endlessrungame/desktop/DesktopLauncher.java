74f9178c0874c14505510757f0b18d507ba11a48
Burhan ELGÜN <belgun@gtu.edu.tr>
2018-08-20 18:08:00 +0300

    First Library
    Library is loaded.

---
+++ desktop/src/com/bestudios/endlessrungame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bestudios.endlessrungame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bestudios.endlessrungame.EndlessRunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EndlessRunGame(), config);
+	}
+}
